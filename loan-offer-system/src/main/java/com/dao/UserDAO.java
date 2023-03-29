a0b5c1c4ab06ae7b0323420e8eabe4ff218998d3
Akalya97 <akalya.gnanasekaram@gmail.com>
2023-03-30 01:07:14 +0530

    Update UserDAO.java

--- loan-offer-system/src/main/java/com/dao/UserDAO.java
+++ loan-offer-system/src/main/java/com/dao/UserDAO.java
@@ -3,6 +3,10 @@ package com.dao;
 import com.dto.response.GeneralResponse;
 import com.dto.response.LoanOfferResponse;
 import com.dto.user.request.CreateNewUserRequest;
+import com.dto.user.request.GetCustomerDetailReq;
+import com.dto.user.response.CustomerRes;
+
+import java.util.List;
 
 public interface UserDAO {
 
@@ -12,4 +16,18 @@ public interface UserDAO {
      * @return
      */
     GeneralResponse createNewUser(CreateNewUserRequest createNewUserReq);
+
+    /**
+     * getCustomerDetail
+     * @param getCustomerDetailReq
+     * @return
+     */
+    CustomerRes getCustomerDetail(GetCustomerDetailReq getCustomerDetailReq);
+
+
+    /**
+     * getCustomerList
+     * @return
+     */
+    List<CustomerRes> getCustomerList();
 }
