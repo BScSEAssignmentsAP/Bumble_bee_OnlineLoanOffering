1244d237958adc8c9df4e2b7a61cf973bb265c55
Akalya97 <akalya.gnanasekaram@gmail.com>
2023-03-30 01:06:33 +0530

    Update UserControllerImpl.java

--- loan-offer-system/src/main/java/com/controller/impl/UserControllerImpl.java
+++ loan-offer-system/src/main/java/com/controller/impl/UserControllerImpl.java
@@ -2,13 +2,20 @@ package com.controller.impl;
 
 import com.business.UserBusiness;
 import com.controller.UserController;
+import com.dto.response.GeneralResponse;
 import com.dto.response.LoanOfferResponse;
 import com.dto.user.request.CreateNewUserRequest;
+import com.dto.user.request.GetCustomerDetailReq;
+import com.dto.user.response.CustomerRes;
+import com.util.ApplicationConstant;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+//import java.util.List;
 
 @RestController
 public class UserControllerImpl implements UserController {
@@ -19,7 +26,26 @@ public class UserControllerImpl implements UserController {
     @Override
     @PostMapping("/create/new/user")
     public LoanOfferResponse createNewUser(@RequestBody CreateNewUserRequest createNewUserReq) {
-        userBusiness.createNewUser(createNewUserReq);
-        return null;
+        GeneralResponse response = userBusiness.createNewUser(createNewUserReq);
+        return LoanOfferResponse.generateResponse(null,response.getStatusCode(),response.getMsg());
+    }
+
+    @Override
+    @PostMapping("/get/customer/detail")
+    public LoanOfferResponse getCustomerDetail(@RequestBody GetCustomerDetailReq getCustomerDetailReq) {
+        CustomerRes customerDetail =userBusiness.getCustomerDetail(getCustomerDetailReq);
+        return LoanOfferResponse.generateResponse(customerDetail,
+                ApplicationConstant.SuccessStatusCode,
+                ApplicationConstant.SuccessMsg);
+
+    }
+
+    @Override
+    @PostMapping("/get/customer/list")
+    public LoanOfferResponse getCustomerList() {
+        List<CustomerRes> customerResList = userBusiness.getCustomerList();
+        return LoanOfferResponse.generateResponse(customerResList,
+                                                    ApplicationConstant.SuccessStatusCode,
+                                                    ApplicationConstant.SuccessMsg);
     }
 }
