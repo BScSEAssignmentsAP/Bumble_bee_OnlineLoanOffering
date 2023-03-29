e342fd6521bfc9090b88d1f8a69e978e6feecc0c
Akalya97 <akalya.gnanasekaram@gmail.com>
2023-03-30 01:06:05 +0530

    Update UserBusinessImpl.java
    customer details view, connection object release, exception handling, calculation time of connect API

--- loan-offer-system/src/main/java/com/business/impl/UserBusinessImpl.java
+++ loan-offer-system/src/main/java/com/business/impl/UserBusinessImpl.java
@@ -5,8 +5,12 @@ import com.dao.UserDAO;
 import com.dto.response.GeneralResponse;
 import com.dto.response.LoanOfferResponse;
 import com.dto.user.request.CreateNewUserRequest;
+import com.dto.user.request.GetCustomerDetailReq;
+import com.dto.user.response.CustomerRes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserBusinessImpl implements UserBusiness {
@@ -16,5 +20,15 @@ public class UserBusinessImpl implements UserBusiness {
     @Override
     public GeneralResponse createNewUser(CreateNewUserRequest createNewUserReq) {
         return userDAO.createNewUser(createNewUserReq);
+    }
+
+    @Override
+    public CustomerRes getCustomerDetail(GetCustomerDetailReq getCustomerDetailReq) {
+        return userDAO.getCustomerDetail(getCustomerDetailReq);
+    }
+
+    @Override
+    public List<CustomerRes> getCustomerList() {
+        return userDAO.getCustomerList();
     }
 }
