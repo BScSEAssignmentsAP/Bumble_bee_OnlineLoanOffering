b36eabe68d01cbd597b1e921dbbda2f19e9879fc
Akalya97 <akalya.gnanasekaram@gmail.com>
2023-03-30 01:06:23 +0530

    Update UserBusiness.java

--- loan-offer-system/src/main/java/com/business/UserBusiness.java
+++ loan-offer-system/src/main/java/com/business/UserBusiness.java
@@ -1,8 +1,11 @@
 package com.business;
 
 import com.dto.response.GeneralResponse;
-import com.dto.response.LoanOfferResponse;
 import com.dto.user.request.CreateNewUserRequest;
+import com.dto.user.request.GetCustomerDetailReq;
+import com.dto.user.response.CustomerRes;
+
+import java.util.List;
 
 public interface UserBusiness {
 
@@ -12,4 +15,18 @@ public interface UserBusiness {
      * @return
      */
     GeneralResponse createNewUser(CreateNewUserRequest createNewUserReq);
+
+    /**
+     * getCustomerDetail
+     * @param getCustomerDetailReq
+     * @return
+     */
+    CustomerRes getCustomerDetail(GetCustomerDetailReq getCustomerDetailReq);
+
+
+    /**
+     * getCustomerList
+     * @return
+     */
+    List<CustomerRes> getCustomerList();
 }
